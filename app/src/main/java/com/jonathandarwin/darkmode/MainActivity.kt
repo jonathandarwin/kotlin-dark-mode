bc4af5f0fd1dc5da7cf6c87cefdca35af66e9d7d
jonathanjojo38 <jonathanjojo38@yahoo.com>
2021-02-10 21:29:27 +0700

    remove unnecessary import

--- app/src/main/java/com/jonathandarwin/darkmode/MainActivity.kt
+++ app/src/main/java/com/jonathandarwin/darkmode/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.jonathandarwin.darkmode
 
 import android.content.Context
-import android.content.Intent
-import android.content.SharedPreferences
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.edit
 import kotlinx.android.synthetic.main.activity_main.*

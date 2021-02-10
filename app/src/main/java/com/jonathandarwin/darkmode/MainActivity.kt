9e17f7dc6f750ca135afc160db74166aec43dea1
jonathanjojo38 <jonathanjojo38@yahoo.com>
2021-02-10 21:25:23 +0700

    init

---
+++ app/src/main/java/com/jonathandarwin/darkmode/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.jonathandarwin.darkmode
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.content.res.Configuration
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.edit
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val preference = getSharedPreferences("DarkMode", Context.MODE_PRIVATE)
+
+        // Determine whether the system is on dark mode or not
+        val isSystemDarkMode = resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
+
+        // Get app dark mode configuration
+        val isDark = preference.getBoolean("isDark", isSystemDarkMode)
+        AppCompatDelegate.setDefaultNightMode(if(isDark) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO)
+
+        btn_switch.setOnClickListener {
+            val isDark = preference.getBoolean("isDark", isSystemDarkMode)
+            AppCompatDelegate.setDefaultNightMode(if(isDark) AppCompatDelegate.MODE_NIGHT_NO else AppCompatDelegate.MODE_NIGHT_YES)
+            preference.edit {
+                putBoolean("isDark", !isDark)
+            }
+        }
+    }
+}
